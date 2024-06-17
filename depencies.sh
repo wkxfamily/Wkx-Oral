3cfcb2c9918e4025c6eef102bf311176d81f6dc8
wkxkevin <67488452+wkxkevin@users.noreply.github.com>
2024-06-17 18:48:53 -0500

    Add files via upload

--- depencies.sh
+++ depencies.sh
@@ -1,6 +1,7 @@
-pip3 install pydicom
-pip3 install gdcm
-pip3 install pylibjpeg-libjpeg
-pip3 install opencv-python
-pip3 install pandas
-pip3 install numpy
+brew install pydicom
+brew install gdcm
+pip3 install pydicom --break-system-packages
+pip3 install pillow --break-system-packages
+pip3 install gdcm --break-system-packages
+pip3 install pylibjpeg-libjpeg --break-system-packages
+pip3 install numpy --break-system-packages
--- image_extract.py
+++ image_extract.py
@@ -1,11 +1,9 @@
 import pydicom as dicom
-import matplotlib.pylab as plt
-import cv2
+from PIL import Image
+import numpy as np
 import os
 
-
-# specify your image path
-s_dir = "./wkx_oral/s0000001"
+s_dir = "./s0000001"
 dicom_list = os.listdir(s_dir)
 dicom_list = sorted(dicom_list)
 try:
@@ -13,9 +11,11 @@ try:
 except:
     pass
 for img in dicom_list:
-    print(img)
     if(img[-3:] == "425"):
         break
     curr_dir = s_dir + "/" + img
     ds = dicom.dcmread(curr_dir)
-    cv2.imwrite("./images/"+img+".jpg", ds.pixel_array)
+    curr_image = ds.pixel_array.astype(float)
+    scaled_image = np.uint8((np.maximum(curr_image,0) / curr_image.max()) * 255.0)
+    im = Image.fromarray(scaled_image)
+    im.save("./images/"+img+".png")
